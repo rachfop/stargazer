d42208e56970698d8165946e3036e739938b4c0e
Patrick Rachford <prachford@icloud.com>
2023-05-10 17:05:17 -0700

    update for render

--- run_workflow.py
+++ run_workflow.py
@@ -1,13 +1,31 @@
 import asyncio
-
-from temporalio.client import Client
+import os
+from typing import Optional
+from temporalio.client import Client, TLSConfig
 
 from github_activity import TASK_QUEUE_NAME, GitHubRepo
 from github_workflows import GitHubWorkflow
 
 
 async def main() -> None:
-    client = await Client.connect("localhost:7233")
+    with open(os.getenv("TEMPORAL_MTLS_TLS_CERT"), "rb") as f:
+            client_cert = f.read()
+
+    with open(os.getenv("TEMPORAL_MTLS_TLS_KEY"), "rb") as f:
+        client_key = f.read()
+
+
+    server_root_ca_cert: Optional[bytes] = None
+    
+    client = await Client.connect(
+        os.getenv("TEMPORAL_HOST_URL"),
+        namespace=os.getenv("TEMPORAL_NAMESPACE"),
+        tls=TLSConfig(
+            server_root_ca_cert=server_root_ca_cert,
+            client_cert=client_cert,
+            client_private_key=client_key,
+        ),
+    )
 
     recent_reviewers = await client.execute_workflow(
         GitHubWorkflow.run,
